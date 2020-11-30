6319a8058b0af551f98edaabcd361ea7c9fce610
Yingjing Lin <yjlin789@gmail.com>
2020-11-30 13:25:04 -0800

    add perform_login

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,8 +6,7 @@ describe UsersController do
     it "log in an existing user and redirect to homepage" do
       start_count = User.count
       user = users(:ada)
-      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
-      get auth_callback_path(:github)
+      perform_login(user)
       must_redirect_to root_path
       session[:user_id].must_equal user.id
       User.count.must_equal start_count
@@ -16,12 +15,15 @@ describe UsersController do
     it "create a new user account and redirect to root path" do
       start_count = User.count
       user = User.new(provider:'github', uid:423123, email: 'a@ada.com', username: "Max")
-      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
-      get auth_callback_path(:github)
+      perform_login(user)
       must_redirect_to root_path
       user = User.find_by(uid: user.uid)
       session[:user_id].must_equal user.id
       User.count.must_equal start_count + 1
+    end
+
+    it "redirect to root path if given invalid info" do
+
     end
   end
 
